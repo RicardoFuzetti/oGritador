ffbb2e3162616532a3980fe612db30dbf5e88163
Ricardo Fuzetti <rickfuzetti12@gmail.com>
2024-08-16 15:32:20 -0300

    feat - audio list

--- main.py
+++ main.py
@@ -16,42 +16,58 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # Carregar variáveis do ambiente
 GUILD_ID = int(os.getenv('GUILD_ID'))
 VOICE_CHANNEL_ID = int(os.getenv('VOICE_CHANNEL_ID'))
-AUDIO_FILE_PATH = os.getenv('AUDIO_FILE_PATH')
+AUDIO_FOLDER_PATH = os.getenv('AUDIO_FOLDER_PATH')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
+def get_random_audio_file(folder_path):
+    """Retorna um caminho de arquivo de áudio aleatório dentro da pasta especificada."""
+    audio_files = [f for f in os.listdir(folder_path) if f.endswith(('.mp3', '.wav'))]
+    if audio_files:
+        return os.path.join(folder_path, random.choice(audio_files))
+    print(f"Nenhum arquivo de áudio encontrado em {folder_path}")
+    return None
 
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user}!')
-    voice_channel = bot.get_channel(VOICE_CHANNEL_ID)
-    if voice_channel:
-        await voice_channel.connect()
-        play_audio.start()
-        print("Função play_audio iniciada.")
-    else:
-        print(f"Canal de voz com ID {VOICE_CHANNEL_ID} não encontrado.")
-
-
-@tasks.loop(seconds=1)
-async def play_audio():
-    await asyncio.sleep(random.randint(1, 10))  # Aguarda entre 1 a 10 segundos
-
     guild = bot.get_guild(GUILD_ID)
     if guild:
-        voice_client = discord.utils.get(bot.voice_clients, guild=guild)
-        if voice_client and voice_client.is_connected():
-            if not voice_client.is_playing():
-                if os.path.isfile(AUDIO_FILE_PATH):
-                    audio_source = discord.FFmpegPCMAudio(AUDIO_FILE_PATH)
-                    voice_client.play(audio_source, after=lambda e: print(f'Finished playing: {AUDIO_FILE_PATH}'))
-                    print(f"Reproduzindo: {AUDIO_FILE_PATH}")
-                else:
-                    print(f"Arquivo de áudio não encontrado: {AUDIO_FILE_PATH}")
+        voice_channel = guild.get_channel(VOICE_CHANNEL_ID)
+        if voice_channel:
+            await voice_channel.connect()
+            play_audio.start()
+            print("Função play_audio iniciada.")
         else:
-            print("voice_client não encontrado ou não está conectado.")
+            print(f"Canal de voz com ID {VOICE_CHANNEL_ID} não encontrado.")
     else:
         print(f"Guilde com ID {GUILD_ID} não encontrada.")
 
+@tasks.loop(seconds=10)
+async def play_audio():
+    # Espera um tempo aleatório entre 1 e 10 segundos
+    await asyncio.sleep(random.randint(1, 10))
+
+    guild = bot.get_guild(GUILD_ID)
+    if not guild:
+        print(f"Guilde com ID {GUILD_ID} não encontrada.")
+        return
+
+    voice_client = discord.utils.get(bot.voice_clients, guild=guild)
+    if not voice_client or not voice_client.is_connected():
+        print("Não estou conectado a um canal de voz.")
+        return
+
+    if voice_client.is_playing():
+        print("Já está tocando áudio.")
+        return
+
+    audio_file = get_random_audio_file(AUDIO_FOLDER_PATH)
+    if audio_file and os.path.isfile(audio_file):
+        audio_source = discord.FFmpegPCMAudio(audio_file)
+        voice_client.play(audio_source, after=lambda e: print(f'Finished playing: {audio_file}'))
+        print(f"Reproduzindo: {audio_file}")
+    else:
+        print(f"Arquivo de áudio não encontrado ou pasta vazia: {AUDIO_FOLDER_PATH}")
 
 @bot.command(name="leave")
 async def leave(ctx):
@@ -60,6 +76,5 @@ async def leave(ctx):
         print("Desconectado do canal de voz.")
     else:
         print("Não estou em um canal de voz.")
-
 
 bot.run(DISCORD_TOKEN)
